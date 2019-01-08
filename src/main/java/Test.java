8cb65da8975830bb6631a20e5eb1718c6406a989
lifeifeixz <lifeifeixz@sina.cn>
2019-01-08 21:20:04 +0800

    增加durid

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,23 +1,35 @@
+import com.alibaba.druid.sql.ast.SQLObject;
 import com.alibaba.druid.sql.ast.SQLStatement;
 import com.alibaba.druid.sql.dialect.mysql.parser.MySqlStatementParser;
 import com.alibaba.druid.sql.dialect.mysql.visitor.MySqlSchemaStatVisitor;
 import com.alibaba.druid.sql.parser.SQLStatementParser;
+import com.alibaba.druid.sql.visitor.SQLASTVisitor;
 import com.alibaba.druid.stat.TableStat;
 
 import java.util.*;
 
 public class Test {
     public static void main(String[] args) {
-        String sql = "select (select userId from dept) from user " +
+        String sql = "select (select userId from dept order by userId) from user " +
                 "where id=121 and name='flysLi' group by name order by age desc;";
         long t = System.currentTimeMillis();
         SQLStatementParser parser = new MySqlStatementParser(sql);
         SQLStatement statement = parser.parseStatement();
         MySqlSchemaStatVisitor visitor = new MySqlSchemaStatVisitor();
         statement.accept(visitor);
+        System.out.println("----子查询-----");
+        List<SQLObject> children = statement.getChildren();
+        for (SQLObject object : children) {
+            SQLStatementParser childParser = new MySqlStatementParser(sql);
+            SQLStatement cStatement = childParser.parseStatement();
+            MySqlSchemaStatVisitor sqlastVisitor = new MySqlSchemaStatVisitor();
+            object.accept(sqlastVisitor);
+            System.out.println(sqlastVisitor.getColumns());
 
 
-
+            System.out.println(object);
+        }
+        System.out.println("----table----");
         Map<TableStat.Name, TableStat> tables = visitor.getTables();
         Set<TableStat.Name> keys = tables.keySet();
         for (TableStat.Name name : keys) {
@@ -25,8 +37,7 @@ public class Test {
             TableStat tableStat = tables.get(name);
             System.out.println(tableStat.getDropCount());
             tableStat.getInsertCount();
-            System.out.println(tableStat);
-            System.out.println("查询次数:"+tableStat.getSelectCount());
+            System.out.println("查询次数:" + tableStat.getSelectCount());
         }
 
         System.out.println("------cloum-------");
@@ -51,6 +62,7 @@ public class Test {
         System.out.println();
         System.out.println("-----------排序字段---------");
         List<TableStat.Column> orderColumns = visitor.getOrderByColumns();
+        System.out.println();
         for (TableStat.Column column : orderColumns) {
             System.out.print(column);
 
